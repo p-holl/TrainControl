40062112f38a59295f4590758df7b40384578458
Philipp Holl <philipp@mholl.de>
2026-08-15 23:34:51 +0200

    Update shuttle speed

--- fpme/train_def.py
+++ fpme/train_def.py
@@ -133,7 +133,7 @@ GUETER =            TrainInfo(None, "🚂/🛲", 1, 0, 0, can_reverse=False, max
 # --- Rail cars ---
 ICE_ =              TrainInfo("ICE", "🚅", .33, max_delay=95, delay_rate=.35, max_speed_in_station=(45, 55, 60), switch_avoidance=.9, max_speed_by_track={'regional': 140})
 S_ =                TrainInfo("S", "Ⓢ", .75, max_delay=30, delay_rate=.2, max_speed_in_station=(50, 55, 60), switch_avoidance=.5)
-SHUTTLE_ =             TrainInfo("Shuttle", "🚉", .6, max_delay=40, delay_rate=.4, max_speed_in_station=(60, 70, 70))
+SHUTTLE_ =             TrainInfo("Shuttle", "🚉", .6, max_delay=40, delay_rate=.4, max_speed_in_station=(65, 65, 70))
 BUS_ =              TrainInfo("Bus", "🚌", .8, max_delay=10, delay_rate=0.1, max_speed_in_station=(70, 70, 70))
 # --- Wagons ---
 INTERREGIO_BLAU =   TrainInfo("IC", "🚉", .3, switch_avoidance=1., max_speed_by_track={'regional': 140})
@@ -151,7 +151,7 @@ E_BW = Train(INTERREGIO_BLAU, "BR 101", "Märklin 37394", 1, (0, 13, 25, 46, 67,
 E_RB = Train(RB_ROT, "BR 146.1", "Märklin 29475", 24, speeds(210, exponent=1.1), 30., supports_mm2=False, stop_by_mm1_reverse=False, img_path="E-Lok DB.png")
 S = Train(S_, "BR 648.2 (LINT 41)", "Märklin 37730", 48, speeds(137, 1.742), 35., img_path="S-Bahn.png",
           functions=(LIGHT, TrainFunction("Innenbeleuchtung", 1, False, (TAG_SPECIAL_LIGHT,), 1., None), SOUND, TrainFunction("Horn", 3, False, (TAG_SPECIAL_SOUND,)), INSTANT_ACCELERATION))
-SHUTTLE = Train(SHUTTLE_, "Shuttle", "Märklin 36967", 65, speeds(140, 1.74), 35., img_path="S-Bahn.png",
+SHUTTLE = Train(SHUTTLE_, "Shuttle", "Märklin 36967", 65, speeds(140, 1.5), 35., img_path="S-Bahn.png",
                 functions=(LIGHT, TrainFunction("Innenbeleuchtung", 1, False, (TAG_SPECIAL_LIGHT,), 1., None), SOUND, TrainFunction("Horn", 3, False, (TAG_SPECIAL_SOUND,)), INSTANT_ACCELERATION))
 BEIGE = Train(SILBERLING, "BR 218", "Märklin 3074", 73, (0, None, 13, 20, 34, 60, 85, 100, 120, 141, 157, 172, 188, 204, 220), 25., img_path="Thumb_BR218_Beige.png",
               functions=(LIGHT, SLOW_MODE, INSTANT_ACCELERATION))
